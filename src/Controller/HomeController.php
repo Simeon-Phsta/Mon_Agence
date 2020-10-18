14522b9396c64adfe91850eec83b7443e1d5c470
Simeon-Phsta <71008690+Simeon-Phsta@users.noreply.github.com>
2020-10-18 10:54:01 +0200

    les routes ne sont plus en yaml mais en annotations + ajout de la bdd

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,10 +8,10 @@ use Twig\Environment;
 
 class HomeController extends AbstractController
 {
-    /*
+    /**
     * @Route("/", name="home")
     * @return Response
-    */
+    **/
     public function index(): Response
     {
         return $this->render('pages/home.html.twig');
--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -1,6 +1,7 @@
 <?php 
 namespace App\Controller;
 
+use App\Entity\Property;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -8,12 +9,15 @@ use Symfony\Component\HttpFoundation\Response;
 
 class PropertyController extends AbstractController
 {
-    /*
+    /**
     * @Route("/biens", name="property.index")
     * @return Response
-    */
+    **/
     public function index(): Response
     {
+        $property = new Property();
+        $property -> setTitle("Mon premier bien");
+        
         return $this->render('property/index.html.twig');
     }
 }
